f1c36023a273f256b52fcd98af13be4e42f374ca
web-py-g <61905059+web-py-g@users.noreply.github.com>
2020-09-25 13:54:33 +0300

    Added animation and scroll

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ $(function(){
     $this.css("width", per + "%");
   });
 });
+
+
+$('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
+
+
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if(window.pageYOffset > 722){
+            header.classList.add('sticky__header')
+        }else{
+            header.classList.remove('sticky__header')
+        }
+    }
+}());
