5ea899e48026f0985c259d868f87804859d34569
web-py-g <61905059+web-py-g@users.noreply.github.com>
2020-09-30 18:07:38 +0300

    Upgrade

--- js/main.js
+++ js/main.js
@@ -9,23 +9,34 @@ $(function(){
 
 
 $('a[href^="#"').on('click', function() {
-
+    const headerElHeight =  document.querySelector('.header').clientHeight;
     let href = $(this).attr('href');
 
     $('html, body').animate({
-        scrollTop: $(href).offset().top
+        scrollTop: $(href).offset().top-headerElHeight
     });
     return false;
 });
 
 
 (function () {
+
+
     const header = document.querySelector('.header');
+    const scroll = document.querySelector('.scroll-up');
+    const empty = document.querySelector('.empty__block');
+
     window.onscroll = () => {
-        if(window.pageYOffset > 722){
+        if(window.pageYOffset > 725){
+
+            scroll.classList.add('scroll__active')
             header.classList.add('sticky__header')
+            empty.classList.add('empty__active')            
         }else{
+            scroll.classList.remove('scroll_active')
             header.classList.remove('sticky__header')
+            empty.classList.remove('empty__active')
+            
         }
     }
 }());
